abfde24963442059a3c154e332945962ba0422e1
Hugo Galván <118332303+HAGC-coder@users.noreply.github.com>
2023-09-13 22:14:46 -0600

    Initial commit

---
+++ src/main/java/com/buildingmgmnt/Edificios/EdificiosApplication.java
@@ -0,0 +1,13 @@
+package com.buildingmgmnt.Edificios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdificiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdificiosApplication.class, args);
+	}
+
+}
